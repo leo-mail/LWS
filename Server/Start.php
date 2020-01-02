8e3001cc5d2f6b7af64c5fe4ea8737e009af243c
Lev Zenin <levzenin@pm.me>
2020-01-02 02:01:44 +0200

    rem lang.ini

--- Server/Start.php
+++ Server/Start.php
@@ -4,6 +4,18 @@ require_once 'FireLion/DataStructures/array.php';
 require_once 'FireLion/DataStructures/xml.php';
 
 $config = FireLion\Data\Structures\XML\ToArray(file_get_contents("config.xml"));
+if( !is_dir("Localization") )
+	mkdir("Localization");
+if( !file_exists("Localization/{$config["locale"]}.ini") )
+	{
+	if( $config["locale"] == "ua" )
+		$config["locale"] == "uk_UA";
+	if( $config["locale"] == "en" )
+		$config["locale"] == "en_GB";
+	if( $config["locale"] == "ru" )
+		$config["locale"] == "ru_UA";
+	file_put_contents("Localization/{$config["locale"]}.ini", file_get_contents("https://raw.githubusercontent.com/leo-mail/LWS-lang-{$config["locale"]}/master/{$config["locale"]}.ini"));
+	}
 ini_set('date.timezone', $config["Timezone"]);
 require_once 'httpserver.php';
 require_once 'ServerLoop.php';
--- Server/lang.php
+++ Server/lang.php
@@ -2,10 +2,18 @@
 function __wl($n)
 {
 	$config = FireLion\Data\Structures\XML\ToArray(file_get_contents("config.xml"));
-	$lang = isset($config["lang"])? $config["lang"]: "ua";
-	if( file_exists('lang.ini')){
-		$l = parse_ini_file('lang.ini',1);
-		$l = isset($l[$lang][$n])? $l[$lang][$n]: $n;
+	$locale = isset($locale)? $config["locale"]: "uk_UA";
+	if( !file_exists("Localization/{$locale}.ini") )
+	{
+	if( $locale == "ua" )
+		$locale == "uk_UA";
+	if( $locale == "en" )
+		$locale == "en_GB";
+	if( $locale == "ru" )
+		$locale == "ru_UA";
+	if( file_exists("Localization/{$locale}.ini")){
+		$l = parse_ini_file("Localization/{$locale}.ini",1);
+		$l = isset($l[$locale][$n])? $l[$locale][$n]: $n;
 		if($n[0]=='e' and $n[1]=='r' and is_int((int)$n[2])){
 			return "<center><h1>".substr($n, 2)."</h1><p>$l</p><hr><sub>Lion Web Server</sub></center>";
 		}
